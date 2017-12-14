010667701c45452fb33094c81f981386ec472e53
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-14 12:04:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,11 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/reverse' do
     erb :reverse
-    
+
   end
 
   post '/reverse' do
-    original_string = params[:string]
+    original_string = params["string"]
     reversed_string = original_string.reverse
     erb :reversed
   end
